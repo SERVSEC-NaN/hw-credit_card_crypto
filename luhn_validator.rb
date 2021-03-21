8ed31f552bcdee3aa1d8aab101a43fb4aa3c18cf
Brandon Duval Benn <me@brandonbenn.xyz>
2021-03-22 02:26:12 +0800

    Implemented validator

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -7,5 +7,9 @@ module LuhnValidator
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
+    ((nums_a.reverse.map.with_index do |digit, index|
+        ((index.odd? ? 2 : 1) * digit)
+          .to_s.chars.map(&:to_i).sum
+      end).sum.modulo(10)).zero?
   end
 end
