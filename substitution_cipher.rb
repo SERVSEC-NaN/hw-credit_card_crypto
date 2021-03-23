8b00dcbae0b7d1a91c712030fe66e29fd6afd408
Brandon Duval Benn <me@brandonbenn.xyz>
2021-03-24 01:09:25 +0800

    Implemented caesar and permutation cipher

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -7,6 +7,7 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using caesar cipher
+      document.to_s.bytes.map { |char| (char + key).chr }.join
     end
 
     # Decrypts String document using integer key
@@ -16,6 +17,7 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using caesar cipher
+      encrypt(document, -key)
     end
   end
 
@@ -27,6 +29,8 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+      perm_table = (0..127).to_a.shuffle(random: Random.new(key))
+      document.to_s.chars.map { |c| perm_table[c.ord].chr }.join
     end
 
     # Decrypts String document using integer key
@@ -36,6 +40,8 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
+      perm_table = (0..127).to_a.shuffle(random: Random.new(key))
+      document.chars.map { |c| perm_table.index(c.ord).chr }.join
     end
   end
 end
